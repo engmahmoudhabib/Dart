f74811aef83fdaafd1b00b0ab35e6617609c5a86
mahmoud <mahmoud@kuwaitnet.dev>
2022-01-25 13:20:10 +0200

    add extension method

--- main.dart
+++ main.dart
@@ -2,16 +2,21 @@ import 'classes/human.dart';
 import 'classes/being.dart';
 import 'classes/bird.dart';
 
+
 void main() {
   Human human = Human.eye();
   Being being = Being(
     name: 'being',
     length: 120.3,
   );
+  String dots = 'heheheh..';
   Bird bird = Bird(name: 'Duck', size: 8.22);
-
+  print(dots.countStringDots());
   print(human.eyeColor);
   print(being.name);
   bird.whoICall();
   bird.chase(bird);
+}
+extension  on String {
+  bool countStringDots() => this.contains('.');
 }
