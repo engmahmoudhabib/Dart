e5bca71ca6e97cdb219202b3ffce257ae4d82d15
mahmoud <mahmoud@kuwaitnet.dev>
2022-05-20 09:28:30 +0300

    add single getter

--- main.dart
+++ main.dart
@@ -3,10 +3,14 @@ import 'sets/sets.dart';
 void main() {
   //? even if repeat any element inside it you'll not get any repeated one
   //? here "Is" repeated twice but only one appear
-  var equal = set.fold(set.first, (previousValue, element) {
+  //* uncomment this line to test fold method
+ /*  var equal = set.fold(set.first, (previousValue, element) {
     print('prev is : $previousValue');
     print('element is : $element');
     return element + (previousValue as int);
-  });
-  print(equal);
+  }); */
+  //print(equal);
+  //? Checks that this iterable has only one element, and returns that element
+  //* uncomment this line to test single getter
+ // print(set.single);
 }
